d714c5be9268c3adb5ce22b7882acfe5a135df18
agusmoncada <agus.monc@gmail.com>
2022-12-07 15:23:59 -0300

    commit

--- part2/course-info/src/components/Part.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Part = ({ part, exercise }) => <p>{part}: {exercise}</p>
-
-export default Part
--- part2/course-info/src/components/Total.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Total = ({ total }) => <p><b>total of {total} exercises</b></p>
-
-export default Total
